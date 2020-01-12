dacb80bba37bea4b695c47e4e4b94b8e951fe151
Alaguraj Veluchamy <alaguraj.v@gmail.com>
2020-01-12 10:54:02 +0000

    Update readfiles.py

--- readfiles.py
+++ readfiles.py
@@ -54,33 +54,3 @@ def makemergedpeaks(argu, file, filesmod, filecount):
         #print fileinthree
         fileinthreeoverlap = open(fileinthree+ "_" +str(filecount), "w")
         call(['bedtools', 'map', '-c', column, '-o', 'mean', '-a',  sortmergedpeakswindowsfile, '-b', fileinthree, '-null', ifnooverlapnull], stdout = fileinthreeoverlap)
-
-    #makearrays(argu.dir_bed)
-        # for line in readfile:
-        #     a=10
-            #print (line)
-            #print (line[4])
-
-
-        #for file in files:
-
-        # parser_t.add_argument('-T', '--tffmfile', required=True, dest='tffm_file',
-        #                       action='store', type=str, help='TFFM XML file.')
-        #
-        # parser_t.add_argument('-t', '--tffm_kind', required=False,
-        #                       dest='tffm_kind', action='store', type=str,
-        #                       choices=['first_order', 'detailed'],0986776666666555
-        #                       default='first_order',
-        #                       help='TFFM kind ("first_order" or "detailed").')
-        # help_str = 'Input fasta file containing the foreground sequences.'
-        # parser_t.add_argument('-i', '--fg_fasta', required=True, type=str,
-        #                       dest='fg_fasta', action='store', help=help_str)
-        # help_str = 'Input bed file w/ positions of foreground sequences.'
-        # parser_t.add_argument('-I', '--fg_bed', required=True, type=str,
-        #                       dest='fg_bed', action='store', help=help_str)
-        # help_str = 'Input fasta containing the background sequences.'
-        # parser_t.add_argument('-b', '--bg_fasta', required=True, type=str,
-        #                       dest='bg_fasta', action='store', help=help_str)
-        # help_str = 'Input bed file w/ positions of background sequences.'
-        # parser_t.add_argument('-B', '--bg_bed', required=True, type=str,
-        #                       dest='bg_bed', action='store', help=help_str)
